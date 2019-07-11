c393ebaaea153c977209524091a204cc554073bb
DavidFarinango1 <davidfarinango24.1995@gmail.com>
2019-07-10 20:42:11 -0500

    fixed the bottom (Agregar al carrito) from each prod.

--- src/components/products/components/description.js
+++ src/components/products/components/description.js
@@ -12,22 +12,24 @@ class Description extends Component {
     constructor(props){
         super(props);
         this.state ={
-            autor_email: '',
-            p_name: '',
-            p_categories: '',
-            p_price: '',
-            p_shortdetails: '',
-            // p_title_general_description1: '',
-            p_details_general_description1: '',
-            // p_title_general_description2: '',
-            p_details_general_description2: '',
-            // p_title_general_description3: '',
-            // p_details_general_description3: '',
-            // p_myubication: '',
-            p_logistic_seller: '',
-            p_principal_image: '',
-            p_secundary_image2: '',
-            p_secundary_image3: '',
+            data: [],
+            id: '',
+            // autor_email: '',
+            // p_name: '',
+            // p_categories: '',
+            // p_price: '',
+            // p_shortdetails: '',
+            // // p_title_general_description1: '',
+            // p_details_general_description1: '',
+            // // p_title_general_description2: '',
+            // p_details_general_description2: '',
+            // // p_title_general_description3: '',
+            // // p_details_general_description3: '',
+            // // p_myubication: '',
+            // p_logistic_seller: '',
+            // p_principal_image: '',
+            // p_secundary_image2: '',
+            // p_secundary_image3: '',
         }
     }
     componentDidMount(){
@@ -38,22 +40,24 @@ class Description extends Component {
                 console.log('documento tomado')
                 if(doc.exists){
                     this.setState({
-                        autor_email: doc.data().autor_email,
-                        p_name: doc.data().p_name,
-                        p_categories: doc.data().p_categories,
-                        p_price: doc.data().p_price ,
-                        p_shortdetails: doc.data().p_shortdetails ,
-                        // p_title_general_description1: doc.data().p_title_general_description1 ,
-                        p_details_general_description1: doc.data().p_details_general_description1 ,
-                        // p_title_general_description2: doc.data().p_title_general_description2 ,
-                        p_details_general_description2: doc.data().p_details_general_description2 ,
-                        // p_title_general_description3: doc.data().p_title_general_description3 ,
-                        // p_details_general_description3: doc.data().p_details_general_description3,
-                        p_myubication: doc.data().p_myubication ,
-                        p_logistic_seller: doc.data().p_logistic_seller ,
-                        p_principal_image: doc.data().p_principal_image ,
-                        p_secundary_image2: doc.data().p_secundary_image2 ,
-                        p_secundary_image3: doc.data().p_secundary_image3 ,
+                        data: doc.data(),
+                        id: doc.id,
+                        // autor_email: doc.data().autor_email,
+                        // p_name: doc.data().p_name,
+                        // p_categories: doc.data().p_categories,
+                        // p_price: doc.data().p_price ,
+                        // p_shortdetails: doc.data().p_shortdetails ,
+                        // // p_title_general_description1: doc.data().p_title_general_description1 ,
+                        // p_details_general_description1: doc.data().p_details_general_description1 ,
+                        // // p_title_general_description2: doc.data().p_title_general_description2 ,
+                        // p_details_general_description2: doc.data().p_details_general_description2 ,
+                        // // p_title_general_description3: doc.data().p_title_general_description3 ,
+                        // // p_details_general_description3: doc.data().p_details_general_description3,
+                        // p_myubication: doc.data().p_myubication ,
+                        // p_logistic_seller: doc.data().p_logistic_seller ,
+                        // p_principal_image: doc.data().p_principal_image ,
+                        // p_secundary_image2: doc.data().p_secundary_image2 ,
+                        // p_secundary_image3: doc.data().p_secundary_image3 ,
                     })
                 }else{
                     console.log('el documento no existe')    
@@ -81,10 +85,10 @@ class Description extends Component {
         return(
             <div>
                 <OneDescription 
-                data={this.state} 
+                proddetails={this.state} 
                 handleOnAddUnit={this.addUnit}
                 />
-                <MoreDescription data={this.state} />
+                {/* <MoreDescription data={this.state} /> */}
                 {/* <Header /> */}
                 {/* <h1 onClick={this.handleClikc}>prueba prueba</h1>
                 <p>Nombre:</p>
--- src/components/products/components/oneprod/onedescription.js
+++ src/components/products/components/oneprod/onedescription.js
@@ -5,21 +5,24 @@ import './onedescription.css';
 class Onedescription extends PureComponent{
     render(){
         return(
+            // <div>
+            //     {this.props.proddetails.data.p_name}
+            // </div>
             <div className="principal-box">
             <div className="PBbox a">
                 <div className="PBdetails-title">Detalles del producto:</div>
-                <div className="PBdetails-description">{this.props.data.p_shortdetails}</div>
+                <div className="PBdetails-description">{this.props.proddetails.data.p_shortdetails}</div>
             </div>
             <div className="PBbox b">
-                <div className="PBproduct prueba">{this.props.data.p_name}</div>
+                <div className="PBproduct prueba">{this.props.proddetails.data.p_name}</div>
                 <div className="PBbox1">
-                    <img src ={this.props.data.p_principal_image} alt="producto" height="40px" weight="40px" />
-                    <img src ={this.props.data.p_secundary_image2} alt="producto" height="40px" weight="40px" />
-                    <img src ={this.props.data.p_secundary_image3} alt="producto" height="40px" weight="40px" />
+                    <img src ={this.props.proddetails.data.p_principal_image} alt="producto" height="40px" weight="40px" />
+                    <img src ={this.props.proddetails.data.p_secundary_image2} alt="producto" height="40px" weight="40px" />
+                    <img src ={this.props.proddetails.data.p_secundary_image3} alt="producto" height="40px" weight="40px" />
                 </div>
                 {/* <div className="PBbox2"> */}
                 <div className="slide-box2 PBbox2">
-                    {/* <img src ={this.props.data.p_principal_image} alt="producto" height="200px" weight="200px" /> */}
+                    {/* <img src ={this.props.proddetails.data.p_principal_image} alt="producto" height="200px" weight="200px" /> */}
                     <div id="carouselindicator1" className="carousel slide slide1a" data-ride="carousel">
                         <ol className="carousel-indicators">
                             <li data-target="#carouselindicator1" data-slide-to="0" className="active"></li>
@@ -28,13 +31,13 @@ class Onedescription extends PureComponent{
                         </ol>
                         <div className="carousel-inner">
                             <div className="carousel-item active">
-                            <img src={this.props.data.p_principal_image} className="d-block" alt="Slide1" />
+                            <img src={this.props.proddetails.data.p_principal_image} className="d-block" alt="Slide1" />
                             </div>
                             <div className="carousel-item">
-                            <img src={this.props.data.p_secundary_image2} className="d-block" alt="Slide2" />
+                            <img src={this.props.proddetails.data.p_secundary_image2} className="d-block" alt="Slide2" />
                             </div>
                             <div className="carousel-item">
-                            <img src={this.props.data.p_secundary_image3} className="d-block" alt="Slide3" />
+                            <img src={this.props.proddetails.data.p_secundary_image3} className="d-block" alt="Slide3" />
                             </div>
                         </div>
                         <a className="carousel-control-prev" href="#carouselindicator1" role="button" data-slide="prev">
@@ -49,17 +52,17 @@ class Onedescription extends PureComponent{
                 </div>
             </div>
             <div className="PBbox c">
-                <div className="PBproduct prueba2">{this.props.data.p_name}</div>
-                <div className="PBprecio"><strong>USD: </strong>{this.props.data.p_price}</div>
-                {/* <div className="PBoferta"><strong>-Por la compra de:</strong>{this.props.data.get('offer')}</div> */}
-                <div className="PBvendido-por"><strong>-Vendido por:</strong>{this.props.data.autor_email}</div>
-                <div className="PBenviado-por"><strong>-Enviado por:</strong>{this.props.data.p_logistic_seller}</div>
+                <div className="PBproduct prueba2">{this.props.proddetails.data.p_name}</div>
+                <div className="PBprecio"><strong>USD: </strong>{this.props.proddetails.data.p_price}</div>
+                {/* <div className="PBoferta"><strong>-Por la compra de:</strong>{this.props.proddetails.data.get('offer')}</div> */}
+                <div className="PBvendido-por"><strong>-Vendido por:</strong>{this.props.proddetails.data.autor_email}</div>
+                <div className="PBenviado-por"><strong>-Enviado por:</strong>{this.props.proddetails.data.p_logistic_seller}</div>
                 <div className="PBformas de pago"><strong>-Formas de pago:</strong> <br />Pago al momento de la entrega</div>
                 {/* <Link to='/ShoppingCart/Logged'> */}
                 <div className="PBbutton-sell">
                     <button 
                     className="PBventa"
-                    onClick={() => this.props.handleOnAddUnit(this.props)}
+                    onClick={() => this.props.handleOnAddUnit(this.props.proddetails)}
                     >
                     Agregar al carrito
                     </button>
