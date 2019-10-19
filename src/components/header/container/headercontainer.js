35b30f654717b6c5c057222bfed2c60c16879d58
DavidFarinango1 <davidfarinango24.1995@gmail.com>
2019-10-18 19:29:59 -0500

    un cambio entrante

--- src/components/header/container/headercontainer.js
+++ src/components/header/container/headercontainer.js
@@ -7,6 +7,7 @@ class HeaderContainer extends Component {
         return(
             <div>
                 <Header  />
+                <p>Cambio entrante</p>
                 <Subheader />
             </div>    
         )
